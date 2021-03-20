8ae837fb9ba5e602af0c32631b4aa4b1758f7e31
miraiomezasu <3792927266@qq.com>
2021-03-20 20:57:30 +0800

    P2676: 60 point

--- P2676.py
+++ P2676.py
@@ -1,23 +1,53 @@
-# 冒泡排序:每次将最大数放到最后面
+# 使用链表
+class ListNode():
+    def __init__(self, val=0, next=None):
+        self.next = next
+        self.val = val
+# 使用插值排序
+
+
+def insert(listnode: ListNode, i: int):
+    # 进行插入(保持序列递增)
+    head = listnode
+    now = listnode.next
+    if(now is None):
+        listnode.next = ListNode(i)
+        return
+    while(now is not None):
+        if(now.val >= i):
+            _new = ListNode(i)
+            head.next = _new
+            _new.next = now
+            return
+        head = now
+        now = now.next
+    _new = ListNode(i)
+    head.next = _new
+
+
 info = [int(i) for i in input().split()]
-nums = []
-for i in range(info[0]):
-    nums.append(int(input()))
-# 数据收取完毕
-count = 0
-_sum = 0
-for i in range(0, info[0]):
-    num = nums[0]
-    for n in range(1, info[0] - count):
-        if(num > nums[n]):
-            # 目标应该前进
-            nums[n - 1] = nums[n]
-        else:
-            nums[n - 1] = num
-            num = nums[n]
-    nums[info[0] - count - 1] = num
-    count += 1
-    _sum += nums[info[0] - count]
-    if(_sum >= info[1]):
-        break
-print(count)
+
+# 用于标记链表的头
+Vhead = ListNode(-1)
+a = int(input())
+head = ListNode(a)
+Vhead.next = head
+sum = a
+size = 1
+for i in range(info[0] - 1):
+    a = int(input())
+    if(sum > info[1]):
+        if(a > Vhead.next.val):
+            # 此时进行判断更新操作
+            sum += a
+            insert(Vhead, a)
+            size += 1
+            while(sum - Vhead.next.val > info[1]):
+                sum -= Vhead.next.val
+                Vhead.next = Vhead.next.next
+                size -= 1
+        continue
+    insert(Vhead, a)
+    size += 1
+    sum += a
+print(size)
