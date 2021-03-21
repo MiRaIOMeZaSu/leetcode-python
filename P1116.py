adbc96857a0eb9e62df679474f57d362cad3418f
miraiomezasu <3792927266@qq.com>
2021-03-21 19:13:07 +0800

    P1116:fuck luogu's stupid input format

--- P1116.py
+++ P1116.py
@@ -1,47 +1,21 @@
-# 使用快排
+# 使用冒泡
 size = int(input())
 
 
+def sort(nums, size):
+    times = 0
+    for i in range(0, size):
+        for n in range(i + 1, size):
+            if(nums[i] > nums[n]):
+                times += 1
+    return times
+
+
 times = 0
+nums = []
+while(size - len(nums) != 0):
+    nums.extend([int(i) for i in input().split()])
 
-
-def quickSort(nums, left, right):
-    if(left < 0 or right >= len(nums)):
-        return
-    if(right <= left):
-        return
-    patitationIndex = sort(nums, left, right)
-    quickSort(nums, left, patitationIndex - 1)
-    quickSort(nums, patitationIndex + 1, right)
-
-
-def sort(nums, left, right):
-    global times
-    pivot = left
-    index = i = left + 1
-    while(i <= right):
-        if(nums[index] <= nums[pivot]):
-            temp = nums[index]
-            nums[index] = nums[i]
-            nums[i] = temp
-            if(i - index <= 1):
-                times += i - index
-            else:
-                times += 2 * (i - index) - 1
-            index += 1
-        i += 1
-
-    temp = nums[pivot]
-    nums[pivot] = nums[index - 1]
-    nums[index - 1] = temp
-    if(index - 1 - pivot <= 1):
-        times += index - 1 - pivot
-    else:
-        times += 2 * (index - 1 - pivot) - 1
-    return index - 1
-
-
-if size > 0:
-    nums = [int(i) for i in input().split()]
-    quickSort(nums, 0, size - 1)
+if(len(nums) > 0):
+    times = sort(nums, size)
 print(times)
