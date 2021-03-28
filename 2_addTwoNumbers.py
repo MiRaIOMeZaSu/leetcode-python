924755e2e7bc0683f074599b46a8655358040958
miraiomezasu <3792927266@qq.com>
2021-03-28 18:41:12 +0800

    2: beautiful example

--- 2_addTwoNumbers.py
+++ 2_addTwoNumbers.py
@@ -5,37 +5,22 @@ class ListNode:
 
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = ListNode()
-        ret = head
-        next = 0
-        while(l1 or l2):
-            val = 0
-            if l1 and l2:
-                val = l1.val + l2.val + next
-                l1 = l1.next
-                l2 = l2.next
-            elif l1:
-                val = l1.val + next
-                l1 = l1.next
-            elif l2:
-                val = l2.val + next
-                l2 = l2.next
-            # 进行赋值
-            head.val = val % 10
-            if(val >= 10):
-                next = 1
-            else:
-                next = 0
-            if(l1 or l2):
-                head.next = ListNode()
-                head = head.next
-        if(next == 1):
-            head.next = ListNode(1)
-        return ret
+        dummy = p = ListNode(None)  # 保存头结点，返回结果
+        s = 0  # 每一步的求和暂存变量
+        while l1 or l2 or s:  # 循环条件：l1 或者l2（没有遍历完成），s(进位)不为0
+            # 这其实是好多代码，我自己写了好多行，但是作者这样写非常简洁，赞
+            s += (l1.val if l1 else 0) + (l2.val if l2 else 0)
+            p.next = ListNode(s % 10)  # 构建新的list存储结果，其实用较长的加数链表存也可以，%10：求个位
+            p = p.next
+            s //= 10  # 求进位
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        # 重要的步骤: 忽视开头用于方便适应第一次循环和之后循环的区别
+        return dummy.next
