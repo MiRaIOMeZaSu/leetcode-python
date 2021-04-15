18fd6d4738b87117995df13b6f1484c87322c7ae
miraiomezasu <3792927266@qq.com>
2021-04-15 11:26:19 +0800

    213:still done

--- 213_rob.py
+++ 213_rob.py
@@ -4,6 +4,7 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # 使用动态规划
+        _max = 0
         if(len(nums) == 1):
             return nums[0]
         dp = []
@@ -27,9 +28,22 @@ class Solution:
                 haveFisrt[i][0] = haveFisrt[i - 1][0] and haveFisrt[i - 1][1]
             dp[i][1] = dp[i - 1][0] + nums[i]
             haveFisrt[i][1] = haveFisrt[i - 1][0]
-        return max(dp[-2][1], dp[-2][0] + (nums[-1] if not haveFisrt[i][0] else 0), dp[-2][0] + nums[-1] - (nums[0] if haveFisrt[i][0] else 0))
+            if(i < len(nums) - 2):
+                _max = max(_max, dp[i][0] +
+                           (0 if haveFisrt[i][0] else nums[-1]),
+                           dp[i][0] +
+                           (nums[-1] - nums[0] if haveFisrt[i][0] else nums[-1]))
+                _max = max(_max, dp[i][1] +
+                           (0 if haveFisrt[i][1] else nums[-1]),
+                           dp[i][1] +
+                           (nums[-1] - nums[0] if haveFisrt[i][1] else nums[-1]))
+        _max = max(_max,
+                   dp[-2][1], dp[-2][0] +
+                   (nums[-1] if not haveFisrt[i][0] else 0),
+                   dp[-2][0] + nums[-1] - (nums[0] if haveFisrt[i][0] else 0))
+        return _max
 
 
 if __name__ == "__main__":
     solution = Solution()
-    solution.rob([1, 1, 3, 6, 7, 10, 7, 1, 8, 5, 9, 1, 4, 4, 3])
+    solution.rob([2, 1, 2, 6, 1, 8, 10, 10])
