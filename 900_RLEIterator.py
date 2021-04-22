ac4b0112a4cfed598320226cea6ef6c4789258e8
miraiomezasu <3792927266@qq.com>
2021-04-22 20:33:53 +0800

    900:wrong answer

---
+++ 900_RLEIterator.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class RLEIterator:
+
+    def __init__(self, A: List[int]):
+        self.d = {}
+        self.arr = []
+        self.sum = 0
+        for i in range(1, len(A), 2):
+            self.arr.append(A[i])
+            self.d[A[i]] = A[i - 1]
+            self.sum += A[i - 1]
+
+    def next(self, n: int) -> int:
+        if len(self.arr) <= 0:
+            return -1
+        if n > self.sum:
+            return -1
+        if n == self.d[self.arr[0]]:
+            self.d.pop(self.arr[0])
+            ret = self.arr[0]
+            self.arr.pop(0)
+            self.sum -= n
+            return ret
+        elif n < self.d[self.arr[0]]:
+            self.d[self.arr[0]] -= n
+            self.sum -= n
+            return self.arr[0]
+        else:
+            n -= self.d[self.arr[0]]
+            self.sum -= self.d[self.arr[0]]
+            self.d.pop(self.arr[0])
+            self.arr.pop(0)
+            return self.next(n)
+
+    # Your RLEIterator object will be instantiated and called as such:
+    # obj = RLEIterator(A)
+    # param_1 = obj.next(n)
+
+
+if __name__ == "__main__":
+    pass
