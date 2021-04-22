a5c85316dfee8d3a7a568a9731fb6aee3aae2531
miraiomezasu <3792927266@qq.com>
2021-04-22 08:32:30 +0800

    212:still TLE

--- 212_findWords.py
+++ 212_findWords.py
@@ -1,59 +1,65 @@
 from typing import List
 
 
-# class TreeNode:
-#     def __init__(self, char):
-#         self.char = char
-#         self.next = [None for i in range(26)]
-#         self.pres = None
-
-
-# class TriTree:
-#     pass
+class TreeNode:
+    def __init__(self, char='', key=-1):
+        self.char = char
+        self.key = key
+        self.next = dict()
+        self.isEnd = False
 
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        # 由于word字符数量过大,应该使用前缀树
-        # 直接对表格制作字典树
-        # 直接使用表格进行回溯
+        # 由于word字符数量过大,应该使用前缀树?
+        # 为输入字符(要寻找的字符)构造前缀树
+        # 树构造完毕后在进行遍历
+        self.ret = []
+        self.finded = set()
         self.m = len(board)
         self.n = len(board[0])
-        self.dict = board
-        ret = []
+        self.board = board
         self.table = [[] for i in range(26)]
         for i in range(len(board)):
             for j in range(len(board[0])):
                 self.table[ord(board[i][j]) - 97].append([i, j])
+        tree = TreeNode()
+        # 先构造树
         for word in words:
-            if(self.search(word)):
-                ret.append(word)
-        return ret
+            curr = tree
+            for i in range(len(word)):
+                key = ord(word[i]) - 97
+                if(key not in curr.next):
+                    curr.next[key] = TreeNode(word[i], key)
+                curr = curr.next[key]
+            curr.isEnd = True
+        # 深度优先?广度优先?
+        self.visit = set()
+        for node in tree.next.values():
+            for pos in self.table[node.key]:
+                string = self.getString(pos[0], pos[1])
+                visit = set()
+                visit.add(string)
+                self.solve(node.char, pos[0], pos[1], node, visit)
+        return self.ret
 
-    def search(self, word):
-        for pos in self.table[ord(word[0]) - 97]:
-            visit = set()
-            visit.add(self.getString(pos[0], pos[1]))
-            if(self._search(word, 1, pos[0], pos[1], visit)):
-                return True
-        return False
-
-    def _search(self, word, index, i, j, visit):
-        # index表示这次要找的
-        if(index >= len(word)):
-            return True
+    def solve(self, currWord, i, j, node, visit):
+        if node.isEnd:
+            if currWord not in self.visit:
+                self.visit.add(currWord)
+                self.ret.append(currWord)
         arr = [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]]
         for item in arr:
             if item[0] > -1 and item[0] < self.m and item[1] > -1 and item[1] < self.n:
-                if self.dict[item[0]][item[1]] != word[index]:
-                    continue
-                string = self.getString(item[0], item[1])
-                if string not in visit:
-                    visit.add(string)
-                    if(self._search(word, index + 1, item[0], item[1], visit)):
-                        return True
-                    visit.remove(string)
-        return False
+                key_str = self.board[item[0]][item[1]]
+                key = ord(key_str) - 97
+                if key in node.next:
+                    v = self.getString(item[0], item[1])
+                    if v not in visit:
+                        visit.add(v)
+                        self.solve(currWord + key_str,
+                                   item[0], item[1], node.next[key], visit)
+                        visit.remove(v)
 
     def getString(self, i, j):
         return str(i) + "," + str(j)
