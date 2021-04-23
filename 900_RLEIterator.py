c614d24c55dd3c0b41b9fec4f63794da9a85cc75
miraiomezasu <3792927266@qq.com>
2021-04-23 11:05:25 +0800

    900:done

--- 900_RLEIterator.py
+++ 900_RLEIterator.py
@@ -4,33 +4,36 @@ from typing import List
 class RLEIterator:
 
     def __init__(self, A: List[int]):
-        self.d = {}
+        self.d = []
         self.arr = []
         self.sum = 0
         for i in range(1, len(A), 2):
             self.arr.append(A[i])
-            self.d[A[i]] = A[i - 1]
+            self.d.append(A[i - 1])
             self.sum += A[i - 1]
 
     def next(self, n: int) -> int:
         if len(self.arr) <= 0:
             return -1
         if n > self.sum:
+            self.d = []
+            self.arr = []
+            self.sum = 0
             return -1
-        if n == self.d[self.arr[0]]:
-            self.d.pop(self.arr[0])
+        if n == self.d[0]:
+            self.d.pop(0)
             ret = self.arr[0]
             self.arr.pop(0)
             self.sum -= n
             return ret
-        elif n < self.d[self.arr[0]]:
-            self.d[self.arr[0]] -= n
+        elif n < self.d[0]:
+            self.d[0] -= n
             self.sum -= n
             return self.arr[0]
         else:
-            n -= self.d[self.arr[0]]
-            self.sum -= self.d[self.arr[0]]
-            self.d.pop(self.arr[0])
+            n -= self.d[0]
+            self.sum -= self.d[0]
+            self.d.pop(0)
             self.arr.pop(0)
             return self.next(n)
 
