ee8eadb293d8a003e10679e32b5228ce5da9e35b
miraiomezasu <3792927266@qq.com>
2021-04-24 10:08:41 +0800

    1218:slow way

--- 1218_longestSubsequence.py
+++ 1218_longestSubsequence.py
@@ -6,19 +6,20 @@ class Solution:
         # 重点是保持原有顺序,因此应当在寻找指定值时同时对下标进行验证(哈希表)
         # difference是固定给定的
         # 存储每个数的最长,前面往后取
-        dp = [1]
+        dp = {arr[0]: 1}
         ret = 1
         for index in range(1, len(arr)):
-            for j in range(index - 1, -1, -1):
-                if arr[j] == arr[index] - difference:
-                    dp.append(dp[j] + 1)
-                    ret = max(dp[j] + 1, ret)
-                    break
-            if(len(dp) <= index):
-                dp.append(1)
+            if difference == 0:
+                dp.setdefault(arr[index], 0)
+            else:
+                dp.setdefault(arr[index], 1)
+            if arr[index] - difference in dp:
+                dp[arr[index]] = max(
+                    dp[arr[index]], dp[arr[index] - difference] + 1)
+                ret = max(dp[arr[index]], ret)
         return ret
 
 
 if __name__ == "__main__":
     Solution().longestSubsequence(
-        [6, 7, 6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5], -2)
+        [4, 12, 10, 0, -2, 7, -8, 9, -9, -12, -12, 8, 8], 0)
