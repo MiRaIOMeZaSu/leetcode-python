d67e6b4db54b14b19c1dc573b1544b7fa8783a00
miraiomezasu <3792927266@qq.com>
2021-05-03 15:52:42 +0800

    1261:slow way

---
+++ 1261_FindElements.py
@@ -0,0 +1,59 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class FindElements:
+
+    def __init__(self, root: TreeNode):
+        self.root = root
+        # 此处进行恢复
+        root.val = 0
+        if root.left:
+            self.solve(root.left, 1)
+        if root.right:
+            self.solve(root.right, 2)
+
+    def find(self, target: int) -> bool:
+        # 对类似二叉搜索树式的搜索方式
+        return self.search(self.root, target)
+
+    def solve(self, root: TreeNode, x: int):
+        root.val = x
+        if root.left:
+            self.solve(root.left, 2 * x + 1)
+        if root.right:
+            self.solve(root.right, 2 * x + 2)
+
+    def search(self, root: TreeNode, target: int):
+        if root.val < target:
+            if root.left:
+                if(self.search(root.left, target)):
+                    return True
+            if root.right:
+                if(self.search(root.right, target)):
+                    return True
+        elif root.val > target:
+            return False
+        else:
+            return True
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
+
+
+if __name__ == "__main__":
+    root = TreeNode(-1)
+    right = TreeNode(-1)
+    root.right = right
+    FindElements().find(2)
