715bb05e6a614652f79448cf470dadf99fbdad84
miraiomezasu <3792927266@qq.com>
2021-05-03 21:39:21 +0800

    1671:copy

--- 1671_minimumMountainRemovals.py
+++ 1671_minimumMountainRemovals.py
@@ -1,36 +1,34 @@
 from typing import List
+import bisect
+
+
+def get_lis_len_arr(arr):
+    lst = []
+    ans = []
+    for i in range(len(arr)):
+        idx = bisect.bisect_left(lst, arr[i])
+        if idx >= 0 and idx < len(lst):
+            # 下面更新过程不会改变lst的长度,因而不会导致新入的最大arr[i]的ans[i]出现错误
+            lst[idx] = arr[i]
+            ans.append(idx + 1)
+        else:
+            lst.append(arr[i])
+            ans.append(len(lst))
+
+    return ans
 
 
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        # 顺序一趟倒序一趟计算当前下标到头(结尾)的子数组内实现递减(递增)需要删除的数量
-        size = len(nums)
-        dp1 = [1]
-        dp2 = [1 for i in range(size)]
-        for i in range(1, size):
-            dp1.append(1)
-            for j in range(i - 1, -1, -1):
-                if nums[j] < nums[i]:
-                    dp1[-1] = max(dp1[j] + 1, dp1[-1])
-                elif nums[j] == nums[i]:
-                    dp1[-1] = max(dp1[j], dp1[-1])
-                    break
-            # 另一边
-            x = size - 1 - i
-            for j in range(x + 1, size):
-                if nums[j] < nums[x]:
-                    dp2[x] = max(dp2[j] + 1, dp2[x])
-                elif nums[j] == nums[x]:
-                    dp2[x] = max(dp2[j], dp2[x])
-                    break
-        ret = size - 3
-        for i in range(1, size - 1):
-            # 以当前坐标作为山顶
-            if dp1[i] == 1 or dp2[i] == 1:
-                continue
-            length = dp1[i] + dp2[i] - 1
-            ret = min(size - length, ret)
-        return ret
+        arr1 = get_lis_len_arr(nums)
+        arr2 = get_lis_len_arr(nums[::-1])
+
+        max_val = 0
+        n = len(nums)
+        for i in range(1, n - 1):
+            if arr1[i] > 1 and arr2[n - i - 1] > 1:
+                max_val = max(max_val, arr1[i] + arr2[n - i - 1] - 1)
+        return n - max_val
 
 
 if __name__ == "__main__":
