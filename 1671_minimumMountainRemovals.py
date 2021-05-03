d6c3a27e1ae954f2579493ae6515dc3fc53787c7
miraiomezasu <3792927266@qq.com>
2021-05-03 21:33:59 +0800

    1671:still slow

--- 1671_minimumMountainRemovals.py
+++ 1671_minimumMountainRemovals.py
@@ -4,9 +4,10 @@ from typing import List
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         # 顺序一趟倒序一趟计算当前下标到头(结尾)的子数组内实现递减(递增)需要删除的数量
+        size = len(nums)
         dp1 = [1]
-        dp2 = [1]
-        for i in range(1, len(nums)):
+        dp2 = [1 for i in range(size)]
+        for i in range(1, size):
             dp1.append(1)
             for j in range(i - 1, -1, -1):
                 if nums[j] < nums[i]:
@@ -15,21 +16,20 @@ class Solution:
                     dp1[-1] = max(dp1[j], dp1[-1])
                     break
             # 另一边
-            x = len(nums) - 1 - i
-            dp2.insert(0, 1)
-            for j in range(x + 1, len(nums)):
+            x = size - 1 - i
+            for j in range(x + 1, size):
                 if nums[j] < nums[x]:
-                    dp2[0] = max(dp2[-(len(nums) - j)] + 1, dp2[0])
+                    dp2[x] = max(dp2[j] + 1, dp2[x])
                 elif nums[j] == nums[x]:
-                    dp2[0] = max(dp2[-(len(nums) - j)], dp2[0])
+                    dp2[x] = max(dp2[j], dp2[x])
                     break
-        ret = len(nums) - 3
-        for i in range(1, len(nums) - 1):
+        ret = size - 3
+        for i in range(1, size - 1):
             # 以当前坐标作为山顶
             if dp1[i] == 1 or dp2[i] == 1:
                 continue
             length = dp1[i] + dp2[i] - 1
-            ret = min(len(nums) - length, ret)
+            ret = min(size - length, ret)
         return ret
 
 
