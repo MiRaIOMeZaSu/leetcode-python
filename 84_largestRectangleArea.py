030f109a2a9beebd780b1d23cf09fe2c1ef7ef5b
miraiomezasu <3792927266@qq.com>
2021-05-04 21:43:52 +0800

    84: slow way

---
+++ 84_largestRectangleArea.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # 动态规划?
+        # 先排序,再依次左右寻找?
+        # 任意区间的最小值
+        # 移动窗口+小顶堆(延迟删除)(暴力法))
+        # 单调栈法,寻找左右两边第一个比当前位置低的位置
+        size = len(heights)
+        dp1 = [1 for i in range(size)]
+        dp2 = [1 for i in range(size)]
+        stack = [0]
+        for i in range(1, size):
+            while len(stack) > 0 and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            # 此时获得长度
+            dp1[i] = i - stack[-1] if len(stack) > 0 else i + 1
+            # 此处应该还有添加操作!
+            stack.append(i)
+        stack = [size - 1]
+        for i in range(size - 2, -1, -1):
+            while len(stack) > 0 and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            dp2[i] = stack[-1] - i if len(stack) > 0 else size - i
+            stack.append(i)
+        # 此时开始进行遍历计算
+        ret = 0
+        for i in range(0, size):
+            ret = max(ret, heights[i] * (dp1[i] + dp2[i] - 1))
+        return ret
+
+
+if __name__ == "__main__":
+    Solution().largestRectangleArea([2, 4])
