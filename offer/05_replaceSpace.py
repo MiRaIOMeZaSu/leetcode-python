fbf74ee2cd8fc2297356d277adec28d188e07686
miraiomezasu <3792927266@qq.com>
2021-06-17 13:03:28 +0800

    offer05:done

--- offer/05_replaceSpace.py
+++ offer/05_replaceSpace.py
@@ -3,4 +3,13 @@ import re
 
 class Solution:
     def replaceSpace(self, s: str) -> str:
-        return re.sub(" ", "%20", s)
+        ret = ""
+        size = len(s)
+        for index in range(size):
+            ch = s[index]
+            if ch == " ":
+                ret += "%20"
+            else:
+                ret += ch
+            index += 1
+        return ret
