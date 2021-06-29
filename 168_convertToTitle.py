50b62283ab39d196da00b13872c037aa083f1871
miraiomezasu <3792927266@qq.com>
2021-06-29 10:08:00 +0800

    168:wrong answer

---
+++ 168_convertToTitle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        # 每次更改的是最后一位
+        div = 26
+        curr = columnNumber - 1
+        if curr // div >= 26:
+            while curr // div > 26:
+                div *= 26
+        res = ""
+        while curr > 26:
+            index = int(curr // div)
+            res += chr(ord('A') + index - 1)
+            curr = int(curr % div)
+            div /= 26
+        res += chr(ord('A') + curr)
+        return res
+
+
+if __name__ == "__main__":
+    Solution().convertToTitle(701)
